1f485818c8815c277ad456a884963de563c5f03e
Rubens Sertage <rubens.sertage@gmail.com>
2025-06-27 15:43:26 +0000

    Added build file

---
+++ dist/index.js
@@ -0,0 +1,9 @@
+import * as core from "@actions/core";
+
+try {
+    const name = core.getInput('name');
+    const output_value = console.log(`Hello ${name}!`);
+    core.setOutput('greeting', output_value);
+} catch (error) {
+  core.setFailed(error.message);
+}
