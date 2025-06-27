c69721d00cd9b326a0c682926b6c9c2cd3aacf95
Rubens Sertage <rubens.sertage@gmail.com>
2025-06-27 15:55:31 +0000

    built version

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,10 @@
-import * as core from "@actions/core";
+/******/ /* webpack/runtime/compat */
+/******/ 
+/******/ if (typeof __nccwpck_require__ !== 'undefined') __nccwpck_require__.ab = new URL('.', import.meta.url).pathname.slice(import.meta.url.match(/^file:\/\/\/\w:/) ? 1 : 0, -1) + "/";
+/******/ 
+/************************************************************************/
+var __webpack_exports__ = {};
+require('@actions/core');
 
 try {
     const name = core.getInput('name');
