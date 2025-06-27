90ea6cc0d6ffbc30356f131205294557c0c5703d
Rubens Sertage <rubens.sertage@gmail.com>
2025-06-27 14:48:07 +0000

    initial version

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 import * as core from '@actions/core';
 
-const name = core.getInput('name');
-const output_value = console.log(`Hello ${name}!`);
-core.setOutput('greeting', output_value);
+try {
+    const name = core.getInput('name');
+    const output_value = console.log(`Hello ${name}!`);
+    core.setOutput('greeting', output_value);
+} catch (error) {
+  core.setFailed(error.message);
+}
